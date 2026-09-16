a2058ae27a12e680c878d7b86123d2af61dee116
Timur Lubyantsev <timurlub29@gmail.com>
2026-09-15 19:41:38 -0700

    fin

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,8 +11,10 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
-
+    angle_modulo = angle % 360
+    if angle_modulo >= 180:
+        angle_modulo -= 360
+    return angle_modulo
 
 def is_angle_between(first_angle, middle_angle, second_angle):
     """Determines whether an angle is between two other angles.
@@ -29,4 +31,15 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    bound_to_mid: float = bound_to_180(middle_angle - first_angle)
+    bound_to_second: float = bound_to_180(second_angle - first_angle)
+
+    if (((bound_to_mid >= 0)
+         and (bound_to_second >= 0)
+        or (bound_to_mid <= 0)
+         and (bound_to_second <= 0))
+        and (abs(bound_to_second) >= abs(bound_to_mid))):
+        return True
+
+    return False
